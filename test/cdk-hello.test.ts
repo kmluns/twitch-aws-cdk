aecf63844937be1162530242d30721a5f1fc5d05
Kamil Ünsal <kamilunsal@pavza.net>
2023-03-25 01:09:48 +0300

    feat: implemented cdk tests

--- test/cdk-hello.test.ts
+++ test/cdk-hello.test.ts
@@ -1,17 +1,72 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkHello from '../lib/cdk-hello-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as CdkHello from '../lib/cdk-hello-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-hello-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkHello.CdkHelloStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('create DynamoDB qr_code', () => {
+  const app = new cdk.App();
+  const stack = new CdkHello.CdkHelloStack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties('AWS::DynamoDB::Table', {
+    TableName: "qr_code"
+  });
+
+});
+
+test('create DynamoDB user_table', () => {
+    const app = new cdk.App();
+    const stack = new CdkHello.CdkHelloStack(app, 'MyTestStack');
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::DynamoDB::Table', {
+      TableName: "user_table"
+    });
+
+  });
+
+
+test('create User Function', () => {
+  const app = new cdk.App();
+  const stack = new CdkHello.CdkHelloStack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties('AWS::Lambda::Function', {
+    FunctionName: "CreateUser"
+  });
+});
+
+test('create User Rest API', () => {
+  const app = new cdk.App();
+  const stack = new CdkHello.CdkHelloStack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties('AWS::ApiGateway::RestApi', {
+    Name: "User-Service"
+  });
+});
+
+test('create POST Method with Lambda Integration', () => {
+  const app = new cdk.App();
+  const stack = new CdkHello.CdkHelloStack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties('AWS::ApiGateway::Method',{
+    HttpMethod: 'POST',
+    Integration: {
+      IntegrationHttpMethod: "POST"
+    }
+  });
+});
+
+test('create GET Method with Lambda Integration', () => {
+  const app = new cdk.App();
+  const stack = new CdkHello.CdkHelloStack(app, 'MyTestStack');
+  const template = Template.fromStack(stack);
+
+  template.hasResourceProperties('AWS::ApiGateway::Method',{
+    HttpMethod: 'GET',
+    Integration: {
+      IntegrationHttpMethod: "POST"
+    }
+  });
 });
